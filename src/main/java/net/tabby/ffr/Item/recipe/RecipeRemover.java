50788a7a81d702ea6075d8a3204a45d10d57c652
TabbyIshProto <TNTcreeper10.ender@gmail.com>
2024-11-14 01:07:18 +0100

    bone-recipes :3

--- src/main/java/net/tabby/ffr/Item/recipe/RecipeRemover.java
+++ src/main/java/net/tabby/ffr/Item/recipe/RecipeRemover.java
@@ -1,6 +1,7 @@
 package net.tabby.ffr.Item.recipe;
 
 import net.minecraft.block.Block;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.CraftingManager;
@@ -10,7 +11,15 @@ import net.minecraftforge.common.crafting.CraftingHelper;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
 import net.minecraftforge.registries.ForgeRegistry;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class RecipeRemover extends ForgeRegistries {
+    public static final Set<?> UNDESIRED = Stream.of(
+            Items.STONE_PICKAXE, Items.STONE_AXE, Items.STONE_HOE, Items.STONE_SHOVEL, Items.STONE_SWORD,
+            Items.WOODEN_PICKAXE, Items.WOODEN_AXE, Items.WOODEN_HOE, Items.WOODEN_SHOVEL, Items.WOODEN_SWORD
+    ).collect(Collectors.toSet());
 
     /*  thanks to Zamion101 on July 15th, 2017 on Forge-forums for finding out how to remove recipes
         <3 all credit goes to them for designing "RecipeRemover" [not loc] <3
